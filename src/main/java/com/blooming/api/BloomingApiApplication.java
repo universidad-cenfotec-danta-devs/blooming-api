4a515a12ed6c9bf1913a1fd0f2f48efde988f4b5
enzocr <enzoquartino@gmail.com>
2025-02-05 14:06:14 -0600

    JWT implementation files changed

---
+++ src/main/java/com/blooming/api/BloomingApiApplication.java
@@ -0,0 +1,13 @@
+package com.blooming.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloomingApiApplication.class, args);
+	}
+
+}
