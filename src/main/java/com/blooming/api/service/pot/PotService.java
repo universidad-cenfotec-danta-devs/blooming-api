c632fcda217788d8f9af41f49af995b87cc7fc45
enzocr <equartinoz@ucenfotec.ac.cr>
2025-04-21 19:28:58 -0600

    [SPRINT-7][BP-XX] Fixing Feedback Notes

--- src/main/java/com/blooming/api/service/pot/PotService.java
+++ src/main/java/com/blooming/api/service/pot/PotService.java
@@ -1,18 +1,16 @@
 package com.blooming.api.service.pot;
 
-import com.blooming.api.entity.Nursery;
 import com.blooming.api.entity.Pot;
 import com.blooming.api.repository.pot.IPotRepository;
-import com.blooming.api.response.dto.NurseryDTO;
 import com.blooming.api.response.dto.PotDTO;
 import com.blooming.api.utils.ParsingUtils;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class PotService implements IPotService {
@@ -27,10 +25,13 @@ public class PotService implements IPotService {
         try {
             Pot save = potRepository.save(pot);
             return ParsingUtils.toPotDTO(save);
-        } catch (RuntimeException e) {
-            throw new RuntimeException(e);
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalArgumentException("Error al guardar la maceta: los datos proporcionados no son válidos.", e);
+        } catch (Exception e) {
+            throw new RuntimeException("Error inesperado al registrar la maceta.", e);
         }
     }
+
 
     @Override
     public Page<PotDTO> getAllPots(int page, int size) {
@@ -42,5 +43,29 @@ public class PotService implements IPotService {
     @Override
     public Pot getPotById(Long id) {
         return potRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Pot with id " + id + " not found"));
+    }
+
+    @Override
+    @Transactional
+    public void activate(Long potId) {
+        if (!potRepository.existsById(potId)) {
+            throw new EntityNotFoundException("Pot not found with id: " + potId);
+        }
+        int updatedRows = potRepository.activate(potId);
+        if (updatedRows == 0) {
+            throw new IllegalArgumentException("Failed to activate pot with id: " + potId);
+        }
+    }
+
+    @Override
+    @Transactional
+    public void deactivate(Long potId) {
+        if (!potRepository.existsById(potId)) {
+            throw new EntityNotFoundException("Pot not found with id: " + potId);
+        }
+        int updatedRows = potRepository.deactivate(potId);
+        if (updatedRows == 0) {
+            throw new IllegalArgumentException("Failed to deactivate pot with id: " + potId);
+        }
     }
 }
